93656c9b8d5e718699e80a58069a0d428454aee4
unknown <ragnoelectronics@gmail.com>
2019-08-15 15:40:56 +0530

    changed counter

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -97,9 +97,9 @@
         if (!parameters.inline) {
             return {
                 days: createCountdownElt(countdown, parameters, 'simply-days-section'),
-                //hours: createCountdownElt(countdown, parameters, 'simply-hours-section'),
-                //minutes: createCountdownElt(countdown, parameters, 'simply-minutes-section'),
-                //seconds: createCountdownElt(countdown, parameters, 'simply-seconds-section')
+                hours: createCountdownElt(countdown, parameters, 'simply-hours-section'),
+                minutes: createCountdownElt(countdown, parameters, 'simply-minutes-section'),
+                seconds: createCountdownElt(countdown, parameters, 'simply-seconds-section')
             };
         }
 
